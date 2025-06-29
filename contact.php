0568eee2df61301d715832213e7bc755c5be3e33
LEAKONO <leakonoemmanuel3@gmail.com>
2025-06-30 00:55:35 +0300

    Fix contact form validation in contact.php

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php include("includes/headers.php")?>
+<h1>Contact Us</h1>
+<form action="submit.php" action='POST'>
+    <input type="text" name="name" placeholder="Your email here" required><br><br>
+    <input type="email" name="email" placeholder="your name" required><br><br>
+    <textarea name="message" placeholder="Your message"></textarea><br><br>
+    <button type="submit">Send</button>
+</form>
+<?php include("includes/footer.php")?>
